4c8fc29f830b76aa89ff2e0a5e706c2b841e3eef
juniorbarbsa <60676892+juniorbarbsa@users.noreply.github.com>
2023-07-15 22:18:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,28 @@ function verificarResposta1() {
   function verificarResposta2() {
     var resposta2 = document.getElementById("resposta2").value.toLowerCase();
     if (resposta2 === "mapa") {
-      window.location.href = "https://pedidomarla.netlify.app/"; // Redireciona para a página do pedido de namoro
+      // Resposta correta!
+      showFireworks(); // Chama a função para exibir os fogos de artifício
+      setTimeout(function() {
+        window.location.href = "https://pedidomarla.netlify.app/"; // Redireciona para a página do pedido de namoro após alguns segundos
+      }, 5000); // Tempo de espera em milissegundos (5 segundos)
     } else {
       alert("Resposta incorreta! Tente novamente.");
+    }
+  }
+  
+  function showFireworks() {
+    // Cria um elemento <div> para cada fogos de artifício
+    for (var i = 0; i < 20; i++) {
+      var fireworks = document.createElement("div");
+      fireworks.className = "fireworks";
+      document.body.appendChild(fireworks);
+  
+      // Define as posições aleatórias dos fogos de artifício na tela
+      var posX = Math.floor(Math.random() * window.innerWidth);
+      var posY = Math.floor(Math.random() * window.innerHeight);
+      fireworks.style.left = posX + "px";
+      fireworks.style.top = posY + "px";
     }
   }
   
